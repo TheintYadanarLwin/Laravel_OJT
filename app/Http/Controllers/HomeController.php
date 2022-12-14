1d120605b5f45da0097ea4fdb6de4ef38699901e
theintyadanarlwin <scm.theintyadanarlwin@gmail.com>
2022-12-14 10:13:55 +0630

    Show Carousel view without Login

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 
 class HomeController extends Controller
 {
@@ -18,6 +19,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $posts = Post::all();
+        return view('home',compact('posts'));
     }
 }
